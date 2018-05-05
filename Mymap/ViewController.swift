14e3ad48e10915a9f6fc0ef4df74cc2d7c379405
江崎英太 <furusatoup@furusatoUp.local>
2018-05-06 03:51:23 +0900

    first day commit

---
+++ Mymap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mymap
+//
+//  Created by 江崎英太 on 2018/05/06.
+//  Copyright © 2018年 HideEsaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
